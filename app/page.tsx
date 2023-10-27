068402d64e2a7d36229a227cbf101162a33d794e
mololuwa10 <mololuwasegi@yahoo.com>
2023-10-27 21:37:52 +0100

    added-project

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { CardDemo } from "@/components/CardDemo";
+import Header from "@/components/Header";
+import {Button} from "@/components/ui/button"
+import { useEffect, useState } from "react";
+// import { useClient } from 'next/data-client';
+
+export default function Home() {
+  // const apiKey = "a510b4df2afcfd16de1622e732af7987";
+  return (
+  <>
+    <div>
+      <Header />
+    </div>
+    <div>
+      <CardDemo />
+    </div>
+    {
+      weatherInfo && weatherInfo.length && weatherInfo.map((item:any) => {
+        return(
+          <>
+            <h4 key={item.name}></h4>
+          </>
+        )
+      })
+    }
+  </>
+  )
+}
